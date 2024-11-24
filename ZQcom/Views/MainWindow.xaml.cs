eea9ec29f0741aa7a3bbda8e77d1ea5172b36e94
fairy <3475397231@qq.com>
2024-11-24 18:08:15 +0800

    【准备】基本功能已经完备，可以发布了

--- ZQcom/Views/MainWindow.xaml.cs
+++ ZQcom/Views/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ namespace ZQcom.Views
         public MainWindow()
         {
             InitializeComponent();
+            // 由于已经在App.xaml.cs中设置了DataContext，所以这里要注释掉
             //DataContext = new ViewModels.MainViewModel();
         }
     }
