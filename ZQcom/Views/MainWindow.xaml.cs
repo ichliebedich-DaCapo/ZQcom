e842e5edfb6dc95bd1226fa8c10555029d007cad
fairy <3475397231@qq.com>
2024-11-25 01:39:55 +0800

    【搁置】界面先做到这，颜色虽然不好看，但至少有个方向去改，唯一的问题就是这个界面的圆角边框有些难搞。

--- ZQcom/Views/MainWindow.xaml.cs
+++ ZQcom/Views/MainWindow.xaml.cs
@@ -11,15 +11,33 @@ namespace ZQcom.Views
             // 由于已经在App.xaml.cs中设置了DataContext，所以这里要注释掉
             //DataContext = new ViewModels.MainViewModel();
         }
+
+        // 最小化窗口
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
 
+        // 最大化窗口
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        // 关闭窗口
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
+        // 窗口标题栏拖动
         private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
