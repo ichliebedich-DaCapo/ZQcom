24b0d68a6cb4af7294e949a07f10e0ea607ee18e
fairy <3475397231@qq.com>
2024-11-25 01:23:45 +0800

    【修改】自定义标题栏

--- ZQcom/Views/MainWindow.xaml.cs
+++ ZQcom/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace ZQcom.Views
 {
@@ -9,6 +10,22 @@ namespace ZQcom.Views
             InitializeComponent();
             // 由于已经在App.xaml.cs中设置了DataContext，所以这里要注释掉
             //DataContext = new ViewModels.MainViewModel();
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
