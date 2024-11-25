61abea92fe93fadb49d99a6511caeea47885ff7e
fairy <3475397231@qq.com>
2024-11-26 00:59:35 +0800

    【准备】尝试ScottPlot，LiveCharts实在是太卡了

--- ZQcom/Views/DataDisplayChartWindow.xaml.cs
+++ ZQcom/Views/DataDisplayChartWindow.xaml.cs
@@ -1,21 +1,37 @@
-﻿using LiveCharts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
+using ScottPlot;
+using ZQcom.ViewModels;
+using ScottPlot.WPF;
 
 namespace ZQcom.Views
 {
-    public partial class DataDisplayChartWindow: Window
+    public partial class DataDisplayChartWindow : Window
     {
-
-
         public DataDisplayChartWindow()
         {
             InitializeComponent();
+            Loaded += Window_Loaded;
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var viewModel = DataContext as ChartViewModel;
+            if (viewModel != null)
+            {
+                // 获取数据
+                var data = viewModel.DataDisplayChartValues.ToArray();
+
+                // 生成X轴数据
+                double[] xData = Enumerable.Range(0, data.Length).Select(i => (double)i).ToArray();
+
+                // 添加数据到现有的 WpfPlot 控件
+                plot.Plot.Add.Scatter(xData, data);// 真他宝贝沙雕，找半天API才找到，API名称就他宝贝这么喜欢变吗
+                plot.Plot.XLabel("Index");
+                plot.Plot.YLabel("Value");
+                // 刷新 WpfPlot 控件以显示更新后的图表
+                plot.Refresh();
+            }
+        }
     }
 }
