4b427702ece76ebe74040fab4ef8880336fb5fea
fairy <3475397231@qq.com>
2024-12-08 00:04:23 +0800

    数据打印窗口，初试FFT功能

--- ZQcom/Views/DataDisplayChartWindow.xaml.cs
+++ ZQcom/Views/DataDisplayChartWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using ScottPlot;
 using ZQcom.ViewModels;
 using ScottPlot.WPF;
+using MathNet.Numerics.IntegralTransforms;
 
 namespace ZQcom.Views
 {
@@ -10,6 +13,9 @@ namespace ZQcom.Views
     {
         private List<double> _dataValues;
         private List<double> _signIndex;
+        private bool _isFFTDisplayed = false;
+        private ScottPlot.Plottables.BarPlot _fftSeries;
+
         public DataDisplayChartWindow(List<double> dataValues, List<double> signIndex)
         {
             _dataValues = dataValues;
@@ -17,7 +23,6 @@ namespace ZQcom.Views
 
             InitializeComponent();
             Loaded += Window_Loaded;
-
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -47,5 +52,78 @@ namespace ZQcom.Views
             // 刷新 WpfPlot 控件以显示更新后的图表
             plot.Refresh();
         }
+
+        private void FFTButton_Click(object sender, RoutedEventArgs e)
+        {
+            int startIndex = int.Parse(StartIndexInput.Text);
+            int length = int.Parse(LengthInput.Text);
+
+            if (startIndex == -1 && length == -1)
+            {
+                startIndex = 0;
+                length = _dataValues.Count;
+            }
+            else if (length > _dataValues.Count - startIndex || startIndex >= _dataValues.Count)
+            {
+                MessageBox.Show("无效的起始位置或长度。");
+                return;
+            }
+
+            var fftData = _dataValues.Skip(startIndex).Take(length).ToArray();
+            var fftResult = ComputeFFT(fftData);
+
+            // Toggle display of FFT result
+            if (_isFFTDisplayed)
+            {
+                RemoveFFTPlot();
+            }
+            else
+            {
+                DisplayFFT(fftResult);
+            }
+
+            _isFFTDisplayed = !_isFFTDisplayed;
+        }
+
+        private float[] ComputeFFT(double[] data)
+        {
+            var complexData = new MathNet.Numerics.Complex32[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                complexData[i] = new MathNet.Numerics.Complex32((float)data[i], 0);
+            }
+
+            Fourier.Forward(complexData, FourierOptions.Matlab);
+
+            var magnitudes = complexData.Select(c => c.Magnitude).ToArray();
+
+            return magnitudes;
+        }
+
+        private void DisplayFFT(float[] fftResult)
+        {
+            List<double> xData = Enumerable.Range(0, fftResult.Length).Select(i => (double)i).ToList();
+            _fftSeries = plot.Plot.Add.Bars(xData, fftResult);
+            plot.Refresh();
+        }
+
+        private void RemoveFFTPlot()
+        {
+            if (_fftSeries != null)
+            {
+                plot.Plot.Remove(_fftSeries);
+                _fftSeries = null;
+            }
+            plot.Refresh();
+        }
+
+        private void SmoothButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Placeholder for smooth button functionality
+            MessageBox.Show("Smoothing not yet implemented.");
+        }
     }
 }
+
+
+
